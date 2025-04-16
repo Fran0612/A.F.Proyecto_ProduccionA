4322d1f6ce09bbc787c6bfb77aae1053d035be79
Fran0612 <franciscojva133@gmail.com>
2025-04-15 21:34:05 -0500

    DiseñoFinalA.F

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primary text-white py-4">
       <div className="container mx-auto px-4 text-center">
         <p className="text-sm">
-          © {currentYear} Universidad Nacional de Chimborazo. Creado por Francisco Javier Andino Vaca
+          © {currentYear} Universidad Nacional de Chimborazo. Elaborado por Francisco Andino Vaca Ing.Telecomunicaciones <br /> Tutores del proyecto: <br /> MgSc. José Jinez Docente de Telecomunicaciones <br /> PhD. Antonio Meneses Docente de Telecomunicaciones
         </p>
       </div>
     </footer>
